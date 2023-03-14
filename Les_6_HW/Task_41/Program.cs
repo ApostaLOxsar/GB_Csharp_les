5deff7405a9e8dd14882fe3b750a71e7f5400397
Stepan <stepanmaslov@gmail.com>
2023-03-14 16:07:25 +0300

    add Les_6_HW

---
+++ Les_6_HW/Task_41/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+Console.Clear();
+
+Console.Write("Введите количество чисел: ");
+int numberAmoung = int.Parse(Console.ReadLine());
+
+int countPositivAmoung = 0;
+
+for (int i = 0; i < numberAmoung; i++)
+{
+    Console.Write("Введите чисело: ");
+    int number = int.Parse(Console.ReadLine());
+    if (number > 0) countPositivAmoung++;
+}
+
+Console.Write($"количество чисел больше 0 = {countPositivAmoung}");
