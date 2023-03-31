c4873738f712d2ba2d0b6db07f0c05fadc6b248a
Stepan <stepanmaslov@gmail.com>
2023-03-31 16:44:54 +0300

    Corect matr

--- Test comand for matrix/Program.cs
+++ Test comand for matrix/Program.cs
@@ -1,29 +1,61 @@
-﻿/*using System.Threading;
-
-Console.Clear();
-
-Random randomNumber = new Random();
-string endText = "ApostaL_Oxsar";
-header.TextColor = Color.FromRgba(0, 255, 0, 100);
-
-for (int i = 0; i < 30; i++)
-{
-    int endTextPrintSim = randomNumber.Next(endText.Length);
-    Console.SetCursorPosition(100 - (endText.Length - endTextPrintSim), 0);
-    Console.Write(endText[endTextPrintSim]);
-    Thread.Sleep(50);
-}*/
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 
-string[] colors = Enum.GetNames(typeof(ConsoleColor));
-for (int i = 0; i < colors.Length; i++)
+/*// создаем новые потоки
+Thread myThread1 = new Thread(new ParameterizedThreadStart(Print));
+//Thread myThread2 = new Thread(Print);
+
+
+// запускаем потоки
+myThread1.Start(1233, 12);
+//myThread2.Start("Привет");
+
+
+void Print(object? message, object? message2)
 {
-    Console.WriteLine(colors[i]);
+    Console.WriteLine(message);
+}
+
+ 
+Person tom = new Person(22, 37);
+// создаем новый поток
+Thread myThread = new Thread(tom.Print);
+myThread.Start();
+ 
+record class Person(int Name, int Age)
+{
+    public void Print()
+    {
+        Console.WriteLine($"Name = {Name}");
+        Console.WriteLine($"Age = {Age}");
+    }
+}*/
+
+using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+        BigInteger result = Factorial(n);
+        Console.WriteLine($"Факториал {n} равен {result}");
+        Console.ReadLine();
+    }
+
+    static BigInteger Factorial(int n)
+    {
+        BigInteger result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
 }
 
